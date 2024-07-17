b7ce7cf6d674991ddde83d737a36c5120de2f61c
PoincareRice <jeonsy639710@gmail.com>
2024-07-18 01:27:36 +0900

    웹_상_나이트_규칙_구현

--- src/components/Board.js
+++ src/components/Board.js
@@ -47,16 +47,18 @@ const isValidMove = (piece, from, to, board) => {
     case 'p':
       // 폰의 움직임 검증
       return isValidPawnMove(piece, fromX, fromY, toX, toY, board);
-    /*
-    case 'r':
-      // 룩의 움직임 검증
-      return isValidRookMove(fromX, fromY, toX, toY, board);
+    
+    // case 'r':
+    //   // 룩의 움직임 검증
+    //   return isValidRookMove(fromX, fromY, toX, toY, board);
+
     case 'n':
       // 나이트의 움직임 검증
       return isValidKnightMove(fromX, fromY, toX, toY);
     case 'b':
       // 비숍의 움직임 검증
       return isValidBishopMove(fromX, fromY, toX, toY, board);
+    /*
     case 'q':
       // 퀸의 움직임 검증
       return isValidQueenMove(fromX, fromY, toX, toY, board);
@@ -93,6 +95,82 @@ const isValidPawnMove = (piece, fromX, fromY, toX, toY, board) => {
 
   // 이 외의 경우는 유효하지 않음
   return false;
+};
+/*
+const isValidRookMove = (fromX, fromY, toX, toY, board) =>{
+  // 같은 열에서 수직 이동 검사
+  if (fromX === toX) {
+    const step = fromY < toY ? 1 : -1;
+    for (let y = fromY + step; y !== toY; y += step) {
+      if (board[`${fromX},${y}`] !== null) {
+        return false; // 중간에 다른 기물이 있으면 유효하지 않음
+      }
+    }
+    return true; // 이동 경로가 모두 비어있으면 유효
+  }
+
+  // 같은 행에서 수평 이동 검사
+  if (fromY === toY) {
+    const step = fromX < toX ? 1 : -1;
+    for (let x = fromX + step; x !== toX; x += step) {
+      if (board[`${x},${fromY}`] !== null) {
+        return false; // 중간에 다른 기물이 있으면 유효하지 않음
+      }
+    }
+    return true; // 이동 경로가 모두 비어있으면 유효
+  }
+
+  // 이동 경로가 수직 또는 수평이 아닌 경우는 유효하지 않음
+  return false;
+};
+*/
+
+const isValidKnightMove = (fromX, fromY, toX, toY, board) => {
+  // 나이트의 유효한 움직임을 나타내는 모든 가능한 이동 방향
+  const knightMoves = [
+    { dx: 2, dy: 1 },
+    { dx: 2, dy: -1 },
+    { dx: -2, dy: 1 },
+    { dx: -2, dy: -1 },
+    { dx: 1, dy: 2 },
+    { dx: 1, dy: -2 },
+    { dx: -1, dy: 2 },
+    { dx: -1, dy: -2 },
+  ];
+
+  // 목표 위치와 일치하는 가능한 움직임을 찾음
+  for (let move of knightMoves) {
+    if (fromX + move.dx === toX && fromY + move.dy === toY) {
+      return true;
+    }
+  }
+
+  // 모든 가능한 움직임을 검사했지만 일치하는 움직임이 없으면 유효하지 않음
+  return false;
+};
+
+const isValidBishopMove = (fromX, fromY, toX, toY, board) => {
+  // 출발 위치와 목표 위치가 같은 대각선에 있는지 검사
+  if (Math.abs(fromX - toX) !== Math.abs(fromY - toY)) {
+    return false;
+  }
+
+  // 대각선 방향을 결정
+  const dx = fromX < toX ? 1 : -1;
+  const dy = fromY < toY ? 1 : -1;
+
+  // 대각선 위에 있는 모든 칸을 검사
+  let x = fromX + dx;
+  let y = fromY + dy;
+  while (x !== toX && y !== toY) {
+    if (board[`${x},${y}`] !== null) {
+      return false; // 중간에 다른 기물이 있으면 유효하지 않음
+    }
+    x += dx;
+    y += dy;
+  }
+
+  return true; // 모든 조건을 만족하면 유효
 };
 
 const Board = ({ sendMoveData }) => {
