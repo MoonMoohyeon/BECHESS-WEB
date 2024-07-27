2f409782c66534d8926c42b034660cd740f3f05f
PoincareRice <jeonsy639710@gmail.com>
2024-07-27 17:56:09 +0900

    룩과 비숍 한칸씩 움직이는 오류 발생과 킹에 캐슬링 규칙 적용 미완성

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,38 +4,38 @@ import Piece from './Piece';
 import '../css/app.css';
 
 const initialBoardSetup = {
-  '0,0': { type: 'r', color: 'b', position: '0,0' },
+  '0,0': { type: 'r', color: 'b', position: '0,0', moved: false },
   '1,0': { type: 'n', color: 'b', position: '1,0' },
   '2,0': { type: 'b', color: 'b', position: '2,0' },
-  '3,0': { type: 'q', color: 'b', position: '3,0' },
-  '4,0': { type: 'k', color: 'b', position: '4,0' },
+  '3,0': { type: 'q', color: 'b', position: '3,0', moved: true },
+  '4,0': { type: 'k', color: 'b', position: '4,0', moved: false },
   '5,0': { type: 'b', color: 'b', position: '5,0' },
   '6,0': { type: 'n', color: 'b', position: '6,0' },
-  '7,0': { type: 'r', color: 'b', position: '7,0' },
+  '7,0': { type: 'r', color: 'b', position: '7,0', moved: false },
   '0,1': { type: 'p', color: 'b', position: '0,1' },
   '1,1': { type: 'p', color: 'b', position: '1,1' },
   '2,1': { type: 'p', color: 'b', position: '2,1' },
   '3,1': { type: 'p', color: 'b', position: '3,1' },
+  '4,1': { type: 'p', color: 'b', position: '4,1' },
   '5,1': { type: 'p', color: 'b', position: '5,1' },
   '6,1': { type: 'p', color: 'b', position: '6,1' },
   '7,1': { type: 'p', color: 'b', position: '7,1' },
   '0,6': { type: 'p', color: 'w', position: '0,6' },
   '1,6': { type: 'p', color: 'w', position: '1,6' },
   '2,6': { type: 'p', color: 'w', position: '2,6' },
-  '4,1': { type: 'p', color: 'b', position: '4,1' },
   '3,6': { type: 'p', color: 'w', position: '3,6' },
   '4,6': { type: 'p', color: 'w', position: '4,6' },
   '5,6': { type: 'p', color: 'w', position: '5,6' },
   '6,6': { type: 'p', color: 'w', position: '6,6' },
   '7,6': { type: 'p', color: 'w', position: '7,6' },
-  '0,7': { type: 'r', color: 'w', position: '0,7' },
+  '0,7': { type: 'r', color: 'w', position: '0,7', moved: false },
   '1,7': { type: 'n', color: 'w', position: '1,7' },
   '2,7': { type: 'b', color: 'w', position: '2,7' },
-  '3,7': { type: 'q', color: 'w', position: '3,7' },
-  '4,7': { type: 'k', color: 'w', position: '4,7' },
+  '3,7': { type: 'q', color: 'w', position: '3,7', moved: true },
+  '4,7': { type: 'k', color: 'w', position: '4,7', moved: false },
   '5,7': { type: 'b', color: 'w', position: '5,7' },
   '6,7': { type: 'n', color: 'w', position: '6,7' },
-  '7,7': { type: 'r', color: 'w', position: '7,7' },
+  '7,7': { type: 'r', color: 'w', position: '7,7', moved: false },
 };
 
 //각 기물이 움직일 수 있는지 여부를 나타내는 함수
@@ -48,27 +48,29 @@ const isValidMove = (piece, from, to, board) => {
       // 폰의 움직임 검증
       return isValidPawnMove(piece, fromX, fromY, toX, toY, board);
     
-    // case 'r':
-    //   // 룩의 움직임 검증
-    //   return isValidRookMove(fromX, fromY, toX, toY, board);
+    case 'r':
+      // 룩의 움직임 검증
+      return isValidRookMove(piece, fromX, fromY, toX, toY, board);
 
     case 'n':
       // 나이트의 움직임 검증
-      return isValidKnightMove(fromX, fromY, toX, toY);
+      return isValidKnightMove(fromX, fromY, toX, toY, board);
+
     case 'b':
       // 비숍의 움직임 검증
       return isValidBishopMove(fromX, fromY, toX, toY, board);
-    /*
+    
     case 'q':
       // 퀸의 움직임 검증
-      return isValidQueenMove(fromX, fromY, toX, toY, board);
+      return isValidQueenMove(piece, fromX, fromY, toX, toY, board);
+
     case 'k':
       // 킹의 움직임 검증
       return isValidKingMove(fromX, fromY, toX, toY, board);
     
     default:
       return false;
-    */
+    
   }
 };
 
@@ -89,43 +91,72 @@ const isValidPawnMove = (piece, fromX, fromY, toX, toY, board) => {
 
   // 대각선 공격
   if (Math.abs(toX - fromX) === 1 && toY === fromY + direction &&
-    board[`${toX},${toY}`] && board[`${toX},${toY}`].color !== piece.color) {
+    board[`${toX},${toY}`] && 
+    board[`${toX},${toY}`].color !== piece.color) {
     return true;
   }
 
   // 이 외의 경우는 유효하지 않음
   return false;
 };
-/*
-const isValidRookMove = (fromX, fromY, toX, toY, board) =>{
-  // 같은 열에서 수직 이동 검사
+
+//오류 한 칸 씩만 움직임************
+const isValidRookMove = (piece, fromX, fromY, toX, toY, board) =>{
+  // 위아래 이동 (y축 이동) 검사
   if (fromX === toX) {
     const step = fromY < toY ? 1 : -1;
+    // 이동 중간에 다른 기물이 있으면 유효하지 않음
     for (let y = fromY + step; y !== toY; y += step) {
       if (board[`${fromX},${y}`] !== null) {
-        return false; // 중간에 다른 기물이 있으면 유효하지 않음
+        return false;
       }
     }
+    // 목적지에 같은 팀의 기물이 있는지 확인
+    if (board[`${toX},${toY}`] && board[`${toX},${toY}`].color === board[`${fromX},${fromY}`].color) {
+      return false;
+    }
+
+    //캐슬링에 사용할 부분
+    if (piece.moved === false){
+      piece.moved = true
+    }
+    
     return true; // 이동 경로가 모두 비어있으면 유효
   }
 
-  // 같은 행에서 수평 이동 검사
+  // 좌우 이동 (x축 이동) 검사
   if (fromY === toY) {
     const step = fromX < toX ? 1 : -1;
+    // 이동 중간에 다른 기물이 있으면 유효하지 않음
     for (let x = fromX + step; x !== toX; x += step) {
       if (board[`${x},${fromY}`] !== null) {
-        return false; // 중간에 다른 기물이 있으면 유효하지 않음
+        return false;
       }
     }
-    return true; // 이동 경로가 모두 비어있으면 유효
+    // 목적지에 같은 팀의 기물이 있는지 확인
+    if (board[`${toX},${toY}`] && board[`${toX},${toY}`].color === board[`${fromX},${fromY}`].color) {
+      return false;
+    }
+
+    //캐슬링에 사용할 부분
+    if (piece.moved === false){
+      piece.moved = true
+    }
+
+    return true;
   }
 
   // 이동 경로가 수직 또는 수평이 아닌 경우는 유효하지 않음
   return false;
 };
-*/
+
 
 const isValidKnightMove = (fromX, fromY, toX, toY, board) => {
+  // 목적지에 같은 팀의 기물이 있는지 확인
+  if (board[`${toX},${toY}`] && board[`${toX},${toY}`].color === board[`${fromX},${fromY}`].color) {
+    return false;
+  }
+
   // 나이트의 유효한 움직임을 나타내는 모든 가능한 이동 방향
   const knightMoves = [
     { dx: 2, dy: 1 },
@@ -162,16 +193,72 @@ const isValidBishopMove = (fromX, fromY, toX, toY, board) => {
   // 대각선 위에 있는 모든 칸을 검사
   let x = fromX + dx;
   let y = fromY + dy;
+  // 중간에 다른 기물이 있으면 유효하지 않음
   while (x !== toX && y !== toY) {
     if (board[`${x},${y}`] !== null) {
-      return false; // 중간에 다른 기물이 있으면 유효하지 않음
+      return false;
     }
     x += dx;
     y += dy;
   }
 
+  // 목적지에 같은 팀의 기물이 있는지 확인
+  if (board[`${toX},${toY}`] && board[`${toX},${toY}`].color === board[`${fromX},${fromY}`].color) {
+    return false;
+  }
+
   return true; // 모든 조건을 만족하면 유효
 };
+
+const isValidQueenMove = (piece, fromX, fromY, toX, toY, board) => {
+  // 룩과 비숍의 움직임을 합함
+  if (fromX === toX || fromY === toY){
+    return isValidRookMove(piece, fromX, fromY, toX, toY, board);
+  }
+  else if (Math.abs(fromX - toX) === Math.abs(fromY - toY)){
+    return isValidBishopMove(fromX, fromY, toX, toY, board);
+  }
+  return false;
+};
+
+const isValidKingMove = (fromX, fromY, toX, toY, board) => {
+  const dx = Math.abs(toX - fromX);
+  const dy = Math.abs(toY - fromY);
+
+  // 한 칸 이동
+  if (dx <= 1 && dy <= 1) {
+    // 목적지에 같은 팀의 기물이 있는지 확인
+    if ( board[`${toX},${toY}`] && board[`${toX},${toY}`].color === board[`${fromX},${fromY}`].color) {
+      return false;
+    }
+    return true; 
+  }
+
+  // 캐슬링 조건 검증
+  // if (dx >= 2 && dy === 0) {
+  //   const king = board[`${fromX},${fromY}`];
+  //   if (king.moved) return false;
+
+  //   // 캐슬링 (오른쪽)
+  //   if (toX === 6 && board[`${fromX + 1},${fromY}`] === null && board[`${fromX + 2},${fromY}`] === null) {
+  //     const rook = board[`${7},${fromY}`];
+  //     if (rook && rook.type === 'r' && !rook.moved) {
+  //       return true;
+  //     }
+  //   }
+
+  //   // 캐슬링 (왼쪽)
+  //   if (toX === 2 && board[`${fromX - 1},${fromY}`] === null && board[`${fromX - 2},${fromY}`] === null && board[`${fromX - 3},${fromY}`] === null) {
+  //     const rook = board[`${0},${fromY}`];
+  //     if (rook && rook.type === 'r' && !rook.moved) {
+  //       return true;
+  //     }
+  //   }
+  // }
+
+  return false;
+};
+
 
 const Board = ({ sendMoveData }) => {
   const [board, setBoard] = useState(initialBoardSetup);
@@ -197,6 +284,20 @@ const Board = ({ sendMoveData }) => {
 
       if (isValidMove(draggedPiece, from, to, board)) {
         const newBoard = { ...board };
+        
+        // 캐슬링 시 룩 이동 처리
+        // if (draggedPiece.type === 'k' && Math.abs(parseInt(from.split(',')[0]) - parseInt(to.split(',')[0])) === 2) {
+        //   const y = from.split(',')[1];
+        //   if (parseInt(to.split(',')[0]) === 6) { // 킹 사이드 캐슬링
+        //     newBoard[`5,${y}`] = { ...newBoard[`7,${y}`], position: `5,${y}`, moved: true };
+        //     newBoard[`7,${y}`] = null;
+        //   } else if (parseInt(to.split(',')[0]) === 2) { // 퀸 사이드 캐슬링
+        //     newBoard[`3,${y}`] = { ...newBoard[`0,${y}`], position: `3,${y}`, moved: true };
+        //     newBoard[`0,${y}`] = null;
+        //   }
+        // }
+
+
         newBoard[draggedOverSquare] = draggedPiece;
         delete newBoard[draggedPiece.position];
 
